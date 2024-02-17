f676039b4854e67eb08aa9c73aa76b114b2f5382
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-02-17 17:05:07 +0500

    Разделение верстки на компоненты

---
+++ src/view/index.js
@@ -0,0 +1,18 @@
+import AddNewPointView from './add-new-point-view';
+import EditPointView from './edit-point-view';
+import FilterView from './filter-view';
+import PointListView from './point-list-view';
+import PointView from './point-view';
+import TripInfoView from './trip-info-view';
+import SortView from './sort-view';
+
+
+export {
+  AddNewPointView,
+  EditPointView,
+  FilterView,
+  PointListView,
+  PointView,
+  TripInfoView,
+  SortView
+};
