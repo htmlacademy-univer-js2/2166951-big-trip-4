0b19940f338458298e604e09d1dece29482f0078
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-03-04 16:27:53 +0500

    Fix

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { MSEC_IN_DAY, MSEC_IN_HOUR } from './const';
+import { TimePeriods } from './const';
 
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
@@ -16,9 +16,9 @@ export const formatStringToTime = (date) => dayjs(date).format('HH:mm');
 export const getPointDuration = (dateFrom, dateTo) => {
   const timeDiff = dayjs(dateTo).diff(dayjs(dateFrom));
 
-  if (timeDiff >= MSEC_IN_DAY) {
+  if (timeDiff >= TimePeriods.MSEC_IN_DAY) {
     return dayjs.duration(timeDiff).format('DD[D] HH[H] mm[M]');
-  } else if (timeDiff >= MSEC_IN_HOUR) {
+  } else if (timeDiff >= TimePeriods.MSEC_IN_HOUR) {
     return dayjs.duration(timeDiff).format('HH[H] mm[M]');
   }
   return dayjs.duration(timeDiff).format('mm[M]');
