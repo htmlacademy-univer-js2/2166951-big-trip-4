b22937fd8d2895ecd20681aecefefc8f4527f6d0
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-03-17 22:13:08 +0500

    refactor: sort, filter default checked

--- src/template/filter-template.js
+++ src/template/filter-template.js
@@ -2,9 +2,11 @@ import { capitalize } from '../utils/common';
 
 export const createFilterTemplate = ({filters}) => `
   <form class="trip-filters" action="#" method="get">
-    ${filters.map(({type, isDisabled}) => `
+    ${filters.map(({ type, isDisabled }, filterIndex) => `
       <div class="trip-filters__filter">
-        <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${isDisabled ? 'disabled' : ''}>
+        <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}"
+        ${isDisabled ? 'disabled' : ''}
+        ${filterIndex === 0 ? 'checked' : ''}>
         <label class="trip-filters__filter-label" for="filter-${type}">${capitalize(type)}</label>
       </div>
     `).join('')}
