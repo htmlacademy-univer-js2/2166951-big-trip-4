0ec01f199c304700421ac2c7e04cbe35a720373f
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2024-03-18 14:36:33 +0500

    refactor: filter, sort template

--- src/template/filter-template.js
+++ src/template/filter-template.js
@@ -4,10 +4,18 @@ export const createFilterTemplate = ({filters}) => `
   <form class="trip-filters" action="#" method="get">
     ${filters.map(({ type, isDisabled }, filterIndex) => `
       <div class="trip-filters__filter">
-        <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}"
-        ${isDisabled ? 'disabled' : ''}
-        ${filterIndex === 0 ? 'checked' : ''}>
-        <label class="trip-filters__filter-label" for="filter-${type}">${capitalize(type)}</label>
+        <input
+          id="filter-${type}"
+          class="trip-filters__filter-input  visually-hidden"
+          type="radio"
+          name="trip-filter"
+          value="${type}"
+          ${isDisabled ? 'disabled' : ''}
+          ${filterIndex === 0 ? 'checked' : ''}
+        >
+        <label class="trip-filters__filter-label" for="filter-${type}">
+          ${capitalize(type)}
+        </label>
       </div>
     `).join('')}
     <button class="visually-hidden" type="submit">Accept filter</button>
